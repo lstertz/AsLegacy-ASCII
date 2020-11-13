6ffbc527e26317b2641655f86a2389b984c4c978
Lucas Stertz <lucas@lucasstertz.com>
2020-11-12 23:37:00 -0500

    4: Updated all Tiles to work with TileSet.

--- World/Characters/AbsentCharacter.cs
+++ World/Characters/AbsentCharacter.cs
@@ -4,14 +4,21 @@ namespace AsLegacy
 {
     public static partial class World
     {
+        /// <summary>
+        /// Represents an abstraction of a Character that has no presence within the World.
+        /// Such characters do not exist in a formal sense and can not be interacted with, they 
+        /// are essentially placeholders on the map, to be replaced by actual Characters 
+        /// when appropriate.
+        /// </summary>
         protected class AbsentCharacter : Character
         {
-            public AbsentCharacter(int col, int row) :
-                base(col, row, Color.Transparent, Color.Transparent, 0, true)
-            { }
-
-            public AbsentCharacter(Character charToReplace) :
-                base(charToReplace, Color.Transparent, Color.Transparent, 0, true)
+            /// <summary>
+            /// Constructs a new Abstract Character at the provided row and column on the map.
+            /// </summary>
+            /// <param name="row">The row position of the new Abstract Character.</param>
+            /// <param name="column">The column position of the new Abstract Character.</param>
+            public AbsentCharacter(int row, int column) :
+                base(row, column, Color.Transparent, Color.Transparent, 0, true)
             { }
         }
     }
--- World/Characters/PresentCharacter.cs
+++ World/Characters/PresentCharacter.cs
@@ -4,10 +4,24 @@ namespace AsLegacy
 {
     public static partial class World
     {
+        /// <summary>
+        /// Represents an abstraction of a Character that has a presence within the World.
+        /// Such characters exist in a formal sense and can be interacted with.
+        /// </summary>
         public abstract class PresentCharacter : Character
         {
-            protected PresentCharacter(Character charToReplace, Color glyphColor, int glyph) :
-                base(charToReplace, Color.Black, glyphColor, glyph, false) { }
+            /// <summary>
+            /// Constructs a new Present Character.
+            /// </summary>
+            /// <param name="row">The row position of the new Present Character.</param>
+            /// <param name="column">The column position of the new Present Character.</param>
+            /// <param name="glyphColor">The color of the glyph visually displayed to represent 
+            /// the new Present Character.</param>
+            /// <param name="glyph">The glyph visually displayed to represent 
+            /// the new Present Character.</param>
+            protected PresentCharacter(int row, int column, Color glyphColor, int glyph) :
+                base(row, column, Color.Black, glyphColor, glyph, false)
+            { }
         }
     }
 }
