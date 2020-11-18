04c55aa4d9789f91f1474f69b3dff01d76f071e1
Lucas Stertz <lucas@lucasstertz.com>
2020-11-17 22:57:57 -0500

    5: Moved Player, Path, and Tree out of World.

--- World/Characters/Player.cs
+++ World/Characters/Player.cs
@@ -2,24 +2,21 @@
 
 namespace AsLegacy
 {
-    public static partial class World
+    /// <summary>
+    /// Defines a Player, the focal character, on the World map, of the real-life player as 
+    /// they interact with the game.
+    /// </summary>
+    public class Player : World.PresentCharacter
     {
         /// <summary>
-        /// Defines a Player, the focal character, on the World map, of the real-life player as 
-        /// they interact with the game.
+        /// Constructs a new Player at the provided row and column on the map.
         /// </summary>
-        public class Player : PresentCharacter
+        /// <param name="row">The row position of the new Player.</param>
+        /// <param name="column">The column position of the new Player.</param>
+        public Player(int row, int column) :
+            base(row, column, Color.White, '@')
         {
-            /// <summary>
-            /// Constructs a new Player at the provided row and column on the map.
-            /// </summary>
-            /// <param name="row">The row position of the new Player.</param>
-            /// <param name="column">The column position of the new Player.</param>
-            public Player(int row, int column) :
-                base(row, column, Color.White, '@')
-            {
 
-            }
         }
     }
 }
--- World/Environment/Tree.cs
+++ World/Environment/Tree.cs
@@ -2,45 +2,41 @@
 
 namespace AsLegacy
 {
-    public static partial class World
+    /// <summary>
+    /// Defines a Path, which is an Environment Element that represents a 
+    /// part of a path, as a Tile, on the World map.
+    /// </summary>
+    public class Tree : World.EnvironmentElement
     {
+        private static int typeValue = 0;
+
         /// <summary>
-        /// Defines a Path, which is an Environment Element that represents a 
-        /// part of a path, as a Tile, on the World map.
+        /// Provides the glyph for a tree based on a pseudo-random 
+        /// generation using primes with the increasing typeValue.
         /// </summary>
-        public class Tree : EnvironmentElement
+        /// <returns>The pseudo-random glyph, either 5 or 6.</returns>
+        private static int GetTreeGlyph()
         {
-            private static int typeValue = 0;
+            return (typeValue += 31) % 13 > 7 ? 5 : 6;
+        }
 
-            /// <summary>
-            /// Provides the glyph for a tree based on a pseudo-random 
-            /// generation using primes with the increasing typeValue.
-            /// </summary>
-            /// <returns>The pseudo-random glyph, either 5 or 6.</returns>
-            private static int GetTreeGlyph()
-            {
-                return (typeValue += 31) % 13 > 7 ? 5 : 6;
-            }
+        /// <summary>
+        /// Constructs a new Tree.
+        /// </summary>
+        public Tree() : 
+            base(Color.Black, Color.ForestGreen, GetTreeGlyph(), true)
+        { }
 
-            /// <summary>
-            /// Constructs a new Tree.
-            /// </summary>
-            public Tree() : 
-                base(Color.Black, Color.ForestGreen, GetTreeGlyph(), true)
-            { }
-
-            /// <summary>
-            /// Constructs a new Tree, to replace an existing Environment Element.
-            /// </summary>
-            /// <param name="row">The row position of the new Tree, 
-            /// to be used to replace any existing Environment Element.</param>
-            /// <param name="column">The column position of the new Tree, 
-            /// to be used to replace any existing Environment Element.</param>
-            public Tree(int row, int column) : this()
-            {
-                if (environment != null)
-                    environment.ReplaceWith(row, column, this);
-            }
+        /// <summary>
+        /// Constructs a new Tree, to replace an existing Environment Element.
+        /// </summary>
+        /// <param name="row">The row position of the new Tree, 
+        /// to be used to replace any existing Environment Element.</param>
+        /// <param name="column">The column position of the new Tree, 
+        /// to be used to replace any existing Environment Element.</param>
+        public Tree(int row, int column) : 
+            base(row, column, Color.Black, Color.ForestGreen, GetTreeGlyph(), true)
+        {
         }
     }
 }
