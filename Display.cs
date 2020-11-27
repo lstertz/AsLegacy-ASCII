1d532c421bd7c6f23ddef23908b324fd11530fb3
Lucas Stertz <lucas@lucasstertz.com>
2020-11-26 23:50:46 -0500

    9: Set the frame of the main Display Console.

--- Display.cs
+++ Display.cs
@@ -2,6 +2,7 @@
 using SadConsole;
 using SadConsole.Components;
 using System;
+using System.Collections.Generic;
 using Console = SadConsole.Console;
 
 namespace AsLegacy
@@ -16,8 +17,46 @@ namespace AsLegacy
         public const int MapViewPortHalfWidth = MapViewPortWidth / 2;
         public const int MapViewPortHalfHeight = MapViewPortHeight / 2;
 
+        /// <summary>
+        /// Defines a map of the frame for the main console.
+        /// The map is structured as {x, {y, glyph} }, where a -1 for x or y indicates that 
+        /// the glyph applies for all values of that axis.
+        /// </summary>
+        private static readonly Dictionary<int, Dictionary<int, int>> frameMap =
+            new Dictionary<int, Dictionary<int, int>>()
+            {
+                {0, new Dictionary<int, int>() {
+                    {0, 201}, 
+                    {AsLegacy.Height - 1, 200}, 
+                    {-1, 186} 
+                } },
+                {AsLegacy.Width / 2, new Dictionary<int, int>()
+                {
+                    {0, 203},
+                    {AsLegacy.Height - 1, 202},
+                    {-1, 186}
+                } },
+                {AsLegacy.Width - 1, new Dictionary<int, int>()
+                {
+                    {0, 187},
+                    {AsLegacy.Height - 1, 188},
+                    {-1, 186}
+                } },
+                {-1, new Dictionary<int, int>()
+                {
+                    {0, 205},
+                    {AsLegacy.Height - 1, 205}
+                } }
+            };
+
         private static Display display;
 
+        /// <summary>
+        /// Initializes the Display.
+        /// Required for any display output to be rendered, or for any 
+        /// child consoles to be created for interaction.
+        /// </summary>
+        /// <param name="console">The Console to become the initialized Display's Console.</param>
         public static void Init(Console console)
         {
             if (display == null)
@@ -31,9 +70,13 @@ namespace AsLegacy
 
         private Rectangle MapViewPort => characters.ViewPort;
 
+        /// <summary>
+        /// Constructs a new Display for the given Console.
+        /// </summary>
+        /// <param name="console">The Console to be the new Display's Console.</param>
         private Display(Console console)
         {
-            // Create frame to outline around other child consoles.
+            SetConsoleFrame(console);
             // Create stats for Player stats/inventory/equipment/legacy.
 
             characters = World.Characters;
@@ -57,9 +100,40 @@ namespace AsLegacy
             console.Components.Add(this);
         }
 
-        // TODO :: Update map viewport.
-        // TODO :: Update stats console.
+        /// <summary>
+        /// Sets the frame, defined by the frame map, for the provided Console.
+        /// </summary>
+        /// <param name="console">The Console to which the frame will be added.
+        /// It is expected that this Console's width/height will match the width/height 
+        /// expected by the frame map.</param>
+        private void SetConsoleFrame(Console console)
+        {
+            int width = console.Width;
+            int height = console.Height;
 
+            for (int x = 0; x < width; x++)
+                for (int y = 0; y < height; y++)
+                {
+                    if (frameMap.ContainsKey(x))
+                    {
+                        if (frameMap[x].ContainsKey(y))
+                            console.SetGlyph(x, y, frameMap[x][y]);
+                        else if (frameMap[x].ContainsKey(-1))
+                            console.SetGlyph(x, y, frameMap[x][-1]);
+                    }
+                    else if (frameMap.ContainsKey(-1))
+                    {
+                        if (frameMap[-1].ContainsKey(y))
+                            console.SetGlyph(x, y, frameMap[-1][y]);
+                    }
+                }
+        }
+
+        /// <summary>
+        /// Updates the rendering of the Display, primarily by adjusting viewport centers.
+        /// </summary>
+        /// <param name="console">The Console of the display.</param>
+        /// <param name="delta">The time passed since the last Draw call.</param>
         public override void Draw(Console console, TimeSpan delta)
         {
             characters.CenterViewPortOnPoint(World.Player.Point);
