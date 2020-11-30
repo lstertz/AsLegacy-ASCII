20d6a6d178ffad53ed76c280a3d52f3409fee7d0
Lucas Stertz <lucas@lucasstertz.com>
2020-11-29 23:30:41 -0500

    11: Implemented a basic Beast functionality.

---
+++ World/Characters/Beasts/Beast.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace AsLegacy
+{
+    /// <summary>
+    /// Defines a Beast, which is a Present Character that cannot 
+    /// use Items and has a number of nature-based skills and abilities.
+    /// </summary>
+    public class Beast : World.PresentCharacter
+    {
+        private const int normal = 224;
+
+        protected override int attackGlyph => 229;
+
+        protected override int defendGlyph => 239;
+
+        protected override int normalGlyph => normal;
+
+        public string Name { get; private set; }
+
+        public Beast(int row, int column, string name) : 
+            base(row, column, Color.DarkOrange, normal)
+        {
+            Name = name;
+        }
+    }
+}
