a3ea62b2a3f008c1a1c576343504e603e1510d54
lstertz <lucas@lucasstertz.com>
2020-12-18 00:03:10 -0500

    14: Implemented Character health properties.

--- World/Characters/Beasts/Beast.cs
+++ World/Characters/Beasts/Beast.cs
@@ -3,19 +3,40 @@
 namespace AsLegacy
 {
     /// <summary>
-    /// Defines a Beast, which is a Present CharacterBase that cannot 
+    /// Defines a Beast, which is a Character that cannot 
     /// use Items and has a number of nature-based skills and abilities.
     /// </summary>
     public class Beast : World.Character
     {
         private const int normal = 224;
 
+        /// <summary>
+        /// Defines the glyph to be shown when the Beast is in attack mode.
+        /// </summary>
         protected override int attackGlyph => 229;
 
+        /// <summary>
+        /// Defines the glyph to be shown when the Beast is in defend mode.
+        /// </summary>
         protected override int defendGlyph => 239;
 
+        /// <summary>
+        /// Defines the glyph to be shown when the Beast is in normal mode.
+        /// </summary>
         protected override int normalGlyph => normal;
 
+
+        /// <summary>
+        /// The maximum health of this Beast.
+        /// </summary>
+        public override float MaxHealth => 6.0f;
+
+        /// <summary>
+        /// Constructs a new Beast at the provided row and column on the map.
+        /// </summary>
+        /// <param name="row">The row position of the new Beast.</param>
+        /// <param name="column">The column position of the new Beast.</param>
+        /// <param name="name">The name of the new Beast.</param>
         public Beast(int row, int column, string name) : 
             base(row, column, Color.DarkOrange, normal, name)
         {
