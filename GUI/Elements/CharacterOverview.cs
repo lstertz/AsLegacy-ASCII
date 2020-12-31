cb3db985dd83c2790e96353917981ac85bc42894
lstertz <lucas@lucasstertz.com>
2020-12-31 00:47:35 -0500

    24: Added outline of the Nearby Panel.

---
+++ GUI/Elements/CharacterOverview.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using SadConsole.Components;
+using System;
+
+namespace AsLegacy.GUI.Elements
+{
+    /// <summary>
+    /// Defines a Character Overview GUI Element, which displays a brief summary of 
+    /// the most pertinent details of a Character.
+    /// </summary>
+    public class CharacterOverview : DrawConsoleComponent
+    {
+        public const int OverviewHeight = 4;
+
+        private const int bottomFrameIndex = OverviewHeight - 1;
+        private const int bottomFrameGlyph = 196;
+
+        public World.Character Character { get; set; }
+        public World.Character Viewer { get; set; }
+
+        private int y;
+
+        /// <summary>
+        /// Constructs a new CharacterOverview.
+        /// </summary>
+        /// <param name="y">The local y position of the CharacterOverview in its Console.</param>
+        /// <param name="viewer">The World Character who is viewing the overview.</param>
+        /// <param name="character">The World Character whose details are to be shown.</param>
+        public CharacterOverview(int y, World.Character viewer, 
+            World.Character character = null)
+        {
+            this.y = y;
+
+            Character = character;
+            Viewer = viewer;
+        }
+
+        /// <summary>
+        /// Draws the Meter, with updated cell glyphs and header colors, to its Console.
+        /// </summary>
+        /// <param name="console">The Console to which this Meter draws.</param>
+        /// <param name="delta">The time passed since the last draw.</param>
+        public override void Draw(SadConsole.Console console, TimeSpan delta)
+        {
+            // TODO :: Use actual Character data, or hide entirely if Character is null.
+            console.Print(0, y, "Character Name");
+            console.Print(0, y + 1, "Character Action"); // TODO :: Center
+            console.Print(0, y + 2, "Character Target"); // TODO :: Center
+
+            console.DrawLine(new Point(0, y + bottomFrameIndex), 
+                new Point(console.Width - 1, y + bottomFrameIndex), 
+                Color.White, Color.Black, bottomFrameGlyph);
+        }
+    }
+}
---
+++ GUI/NearbyPanel.cs
@@ -0,0 +1,45 @@
+﻿using SadConsole;
+
+using AsLegacy.GUI.Elements;
+using System.Collections.Generic;
+
+namespace AsLegacy.GUI
+{
+    /// <summary>
+    /// Defines the NearbyPanel aspect of a Display, 
+    /// which is responsible for showing an overview of the Characters near the focus Character.
+    /// </summary>
+    public class NearbyPanel : Console
+    {
+        /// <summary>
+        /// The Character whose location is used to determine other nearby Characters 
+        /// for displaying in the Panel.
+        /// </summary>
+        public World.Character Focus { get; set; }
+        // TODO :: Update the Viewer of all CharacterOverviews when the Focus is set.
+
+        /// <summary>
+        /// Constructs a new NearbyPanel.
+        /// </summary>
+        /// <param name="width">The width of the Panel.</param>
+        /// <param name="height">The height of the Panel.</param>
+        /// <param name="focus">The Character whose location is used to determine 
+        /// other nearby Characters for displaying in the Panel.</param>
+        public NearbyPanel(int width, int height, World.Character focus) : base(width, height)
+        {
+            Focus = focus;
+
+            CharacterOverview co = new CharacterOverview(0, focus);
+            Components.Add(co);
+        }
+
+        public override void Update(System.TimeSpan timeElapsed)
+        {
+            base.Update(timeElapsed);
+            
+            List<World.Character> nearbyCharacters = World.CharactersNear(Focus,
+                Display.MapViewPortHalfWidth, Display.MapViewPortHalfHeight);
+            // TODO :: Use the nearby Characters to update the Components' Characters.
+        }
+    }
+}
