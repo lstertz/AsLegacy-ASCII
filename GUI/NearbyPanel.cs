2f73b43abf2caa96dad442a8d73732e22df19bd5
lstertz <lucas@lucasstertz.com>
2021-01-01 23:02:43 -0500

    24: Implemented Character Overview updating.

--- GUI/NearbyPanel.cs
+++ GUI/NearbyPanel.cs
@@ -15,8 +15,18 @@ namespace AsLegacy.GUI
         /// The Character whose location is used to determine other nearby Characters 
         /// for displaying in the Panel.
         /// </summary>
-        public World.Character Focus { get; set; }
-        // TODO :: Update the Viewer of all CharacterOverviews when the Focus is set.
+        public World.Character Focus 
+        { 
+            get => focus; 
+            set 
+            { 
+                focus = value;
+
+                foreach (CharacterOverview co in Components)
+                    co.Viewer = value;
+            } 
+        }
+        private World.Character focus;
 
         /// <summary>
         /// Constructs a new NearbyPanel.
@@ -27,19 +37,30 @@ namespace AsLegacy.GUI
         /// other nearby Characters for displaying in the Panel.</param>
         public NearbyPanel(int width, int height, World.Character focus) : base(width, height)
         {
-            Focus = focus;
-
-            CharacterOverview co = new CharacterOverview(0, focus);
-            Components.Add(co);
+            this.focus = focus;
         }
 
+        /// <summary>
+        /// Updates the CharacterOverviews of this NearbyPanel with the nearest Characters 
+        /// of the Panel's focus Character.
+        /// </summary>
+        /// <param name="timeElapsed">The time since the last update.</param>
         public override void Update(System.TimeSpan timeElapsed)
         {
             base.Update(timeElapsed);
             
-            List<World.Character> nearbyCharacters = World.CharactersNear(Focus,
+            World.Character[] nearbyCharacters = World.CharactersNear(Focus,
                 Display.MapViewPortHalfWidth, Display.MapViewPortHalfHeight);
-            // TODO :: Use the nearby Characters to update the Components' Characters.
+
+            while (nearbyCharacters.Length > Components.Count)
+                Components.Add(new CharacterOverview(
+                    Components.Count * CharacterOverview.OverviewHeight, Focus));
+
+            for (int c = 0, count = Components.Count; c < count; c++)
+                if (c >= nearbyCharacters.Length)
+                    (Components[c] as CharacterOverview).Character = null;
+                else
+                    (Components[c] as CharacterOverview).Character = nearbyCharacters[c];
         }
     }
 }
