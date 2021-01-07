5214455329227282eac5f9082b104af59a300744
lstertz <lucas@lucasstertz.com>
2021-01-07 01:01:44 -0500

    30: Updated the GUI display to show the ranking.

---
+++ GUI/Panes/StatsPane.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using SadConsole;
+
+namespace AsLegacy.GUI.Panes
+{
+    /// <summary>
+    /// Defines a StatsPane, a Pane dedicated to showing Character-relevant stats.
+    /// </summary>
+    public class StatsPane : Pane
+    {
+        private readonly Ranking ranking;
+
+        public StatsPane(int width, int height) : 
+            base("Stats", "Character data to be shown here.", width, height)
+        {
+            ranking = new Ranking(width, 8)
+            {
+                Position = new Point(0, 15)
+            };
+            Children.Add(ranking);
+        }
+    }
+}
