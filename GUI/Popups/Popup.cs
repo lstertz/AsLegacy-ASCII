8960e67a07bf9647e6aa2525e9a022ebcc900b8f
lstertz <lucas@lucasstertz.com>
2021-01-30 23:02:10 -0500

    41: Added intial Popup implementation.

---
+++ GUI/Popups/Popup.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using SadConsole;
+
+namespace AsLegacy.GUI.Popups
+{
+    /// <summary>
+    /// Defines a Popup, a Console for displaying potentially 
+    /// interactive data as a window displaying on top of other content.
+    /// </summary>
+    public class Popup : Console
+    {
+        public Popup(string title, string content, int width, int height) : base(width, height)
+        {
+            // TODO :: Render a frame around the window.
+
+            Print(width / 2 - title.Length / 2, 0, title, Color.White);
+            Print(0, 2, content, Color.White);
+        }
+    }
+}
