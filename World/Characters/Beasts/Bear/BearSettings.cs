5551a72870e9d37eb0b81797fc7a36c4ae676e4e
lstertz <lucas@lucasstertz.com>
2021-02-20 13:25:14 -0500

    Split up Beast Settings.

---
+++ World/Characters/Beasts/Bear/BearSettings.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace AsLegacy
+{
+    /// <summary>
+    /// Defines a Beast, which is a Character that cannot 
+    /// use Items and has a number of nature-based skills and abilities.
+    /// </summary>
+    public partial class Beast : World.Character
+    {
+        /// <summary>
+        /// Defines the settings of a Bear.
+        /// </summary>
+        protected class BearSettings : Settings
+        {
+            /// <inheritdoc/>
+            public override Color GlyphColor => Color.DarkOrange;
+            /// <inheritdoc/>
+            public override Combat.Legacy InitialLegacy => new Combat.Legacy(12);
+            /// <inheritdoc/>
+            public override string Name => "Bear";
+            
+            /// <inheritdoc/>
+            public override int AttackGlyph => 226;//'Γ'
+            /// <inheritdoc/>
+            public override int DefendGlyph => 66;//'B'
+            /// <inheritdoc/>
+            public override int NormalGlyph => 225;//'ß'
+
+            /// <inheritdoc/>
+            public override float InitialAttackDamage => 4f;
+            /// <inheritdoc/>
+            public override int InitialAttackInterval => 3000;
+            /// <inheritdoc/>
+            public override float InitialBaseMaxHealth => 10f;
+        }
+    }
+}
---
+++ World/Characters/Beasts/Settings.cs
@@ -0,0 +1,21 @@
+﻿namespace AsLegacy
+{
+    public partial class Beast : World.Character
+    {
+        /// <summary>
+        /// Defines the settings of a Beast.
+        /// </summary>
+        protected abstract class Settings : BaseSettings
+        {
+            /// <summary>
+            /// The initial legacy of the Beast.
+            /// </summary>
+            public abstract Combat.Legacy InitialLegacy { get; }
+
+            /// <summary>
+            /// The name of the Beast.
+            /// </summary>
+            public abstract string Name { get; }
+        }
+    }
+}
