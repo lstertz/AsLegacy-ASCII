0eccdba7adac6642a2bc6ff96b49cf5abceff7df
lstertz <lucas@lucasstertz.com>
2021-02-21 01:08:32 -0500

    51: Implmented basic (randomized) movement AI.

--- World/Characters/BasicAI.cs
+++ World/Characters/BasicAI.cs
@@ -1,4 +1,7 @@
-﻿namespace AsLegacy
+﻿using System;
+using System.Collections.Generic;
+
+namespace AsLegacy
 {
     public partial class World
     {
@@ -8,6 +11,8 @@
             /// Defines the BasicAI that performs minimal AI functionality for any Character.
             protected class BasicAI : IAI
             {
+                private const int MoveChance = 100;
+
                 /// <inheritdoc/>
                 public void UpdateModeOf(Character character) { }
 
@@ -15,7 +20,29 @@
                 public void UpdateTargetOf(Character character) { }
 
                 /// <inheritdoc/>
-                public void InitiateActionFor(Character character) { }
+                public void InitiateActionFor(Character character)
+                {
+                    if (character.CurrentAction != null)
+                        return;
+
+                    Random r = new Random();
+                    if (r.Next(MoveChance) == 0)
+                    {
+                        List<Direction> passableDirections = new List<Direction>();
+                        if (IsPassable(character.Row - 1, character.Column))
+                            passableDirections.Add(Direction.Up);
+                        if (IsPassable(character.Row + 1, character.Column))
+                            passableDirections.Add(Direction.Down);
+                        if (IsPassable(character.Row, character.Column - 1))
+                            passableDirections.Add(Direction.Left);
+                        if (IsPassable(character.Row, character.Column + 1))
+                            passableDirections.Add(Direction.Right);    
+
+                        character.MoveInDirection(
+                            passableDirections[r.Next(passableDirections.Count)]);
+                    }
+
+                }
             }
         }
     }
