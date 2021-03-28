e9b766c2c46ce6c80f9a3fe9562c90fa5712ebcc
lstertz <lucas@lucasstertz.com>
2021-03-27 23:58:42 -0400

    63: Completed initial Class implementations.

---
+++ Characters/Passive.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using static AsLegacy.World;
+
+namespace AsLegacy.Characters
+{
+    /// <summary>
+    /// Defines a Passive, a Character Talent that defines a passive affect for a single Character 
+    /// attribute based on the amount of investment in the passive.
+    /// </summary>
+    public record Passive : Talent
+    {
+        /// <summary>
+        /// The affected Character attribute.
+        /// </summary>
+        public Character.Attribute AffectedAttribute { get; private set; }
+
+        private Func<int, float> _algorithm;
+
+        /// <summary>
+        /// Constructs a new <see cref="Passive"/>.
+        /// </summary>
+        /// <param name="affectedAttribute">The affected Character attribute.</param>
+        /// <param name="title">The title of the <see cref="Passive"/>.</param>
+        /// <param name="investmentAlgorithm">The algorithm that calculates the affect 
+        /// to be applied, for a specified investment, to the Character attribute.</param>
+        public Passive(Character.Attribute affectedAttribute, string title,
+            Func<int, float> investmentAlgorithm) : base(title)
+        {
+            AffectedAttribute = affectedAttribute;
+            _algorithm = investmentAlgorithm;
+        }
+
+        /// <inheritdoc/>
+        public override string GetDescription(int investment)
+        {
+            return AffectedAttribute switch
+            {
+                Character.Attribute.BaseHealth =>
+                    $"Base Health +{_algorithm(investment).ToString("N1")}",
+                _ => throw new NotSupportedException($"The specified attribute " +
+                    $"{AffectedAttribute.ToString()} is not supported for a passive description.")
+            };
+        }
+
+        /// <inheritdoc/>
+        public override string GetDescription(int investmentCurrent, int investmentNext)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Calculates the affect to be applied to the Character attribute.
+        /// </summary>
+        /// <param name="investment">The investment to be used to calculate the affect.</param>
+        /// <returns>The calculated affect.</returns>
+        public float GetAffect(int investment)
+        {
+            return _algorithm(investment);
+        }
+    }
+}
---
+++ Characters/Talent.cs
@@ -0,0 +1,51 @@
+﻿namespace AsLegacy.Characters
+{
+    /// <summary>
+    /// Defines a Talent, the basic representation to define a quality of 
+    /// a Character <see cref="Class"/>.
+    /// </summary>
+    public abstract record Talent
+    {
+        /// <summary>
+        /// The unique ID of the <see cref="Talent"/>, to be used when any results of 
+        /// the <see cref="Talent"/> are referenced elsewhere.
+        /// </summary>
+        public int ID => GetHashCode();
+
+        /// <summary>
+        /// The display title of the <see cref="Talent"/>.
+        /// </summary>
+        public string Title { get; private set; }
+
+        /// <summary>
+        /// Constructs a new <see cref="Talent"/>.
+        /// </summary>
+        /// <param name="title">The display title of the <see cref="Talent"/>.</param>
+        public Talent(string title)
+        {
+            Title = title;
+        }
+
+        /// <summary>
+        /// Provides a description of the <see cref="Talent"/> as it should be with the 
+        /// specified investment.
+        /// </summary>
+        /// <param name="investment">The theoretical amount of points invested in 
+        /// the <see cref="Talent"/>, to be used in generating a matching description.</param>
+        /// <returns>A description, for displaying details of the <see cref="Talent"/> when 
+        /// it has the specified investment.</returns>
+        public abstract string GetDescription(int investment);
+
+        /// <summary>
+        /// Provides a description of the <see cref="Talent"/> as would be appropriate when 
+        /// comparing two different theoretical investments.
+        /// </summary>
+        /// <param name="investmentCurrent">The theoretical current amount of points 
+        /// invested in the <see cref="Talent"/>.</param>
+        /// <param name="investmentNext">The theoretical next amount of points 
+        /// (greater or less than the current) invested in the <see cref="Talent"/>.</param>
+        /// <returns>A description, for displaying details of the <see cref="Talent"/> when 
+        /// comparing the provided investments.</returns>
+        public abstract string GetDescription(int investmentCurrent, int investmentNext);
+    }
+}
