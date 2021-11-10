a19fd09e16f957e225a735298394d2d195b12d8b
lstertz <lucas@lucasstertz.com>
2021-11-09 23:11:58 -0500

    110: Proper context registration.

--- ContextualProgramming/App.cs
+++ ContextualProgramming/App.cs
@@ -11,8 +11,8 @@ namespace ContextualProgramming
     /// </summary>
     public static class App
     {
-        private static List<Context> _contexts = new();
-        private static List<object> _independentBehaviors = new();
+        private static readonly Dictionary<Type, List<Context>> Contexts = new();
+        private static readonly List<object> IndependentBehaviors = new();
 
 
         /// <summary>
@@ -43,7 +43,31 @@ namespace ContextualProgramming
         /// <param name="context">The context to be registered.</param>
         public static void Register(Context context)
         {
-            _contexts.Add(context);
+            Type type = context.GetType();
+
+            Type[] interfaces = type.GetInterfaces();
+            for (int c = 0, count = interfaces.Length; c < count; c++)
+                Register(type, context);
+
+            while (!type.IsAbstract && type != typeof(Context))
+            {
+                Register(type, context);
+                type = type.BaseType;
+            }
+        }
+
+
+        /// <summary>
+        /// Registers a context of the specified type for the execution context of the application.
+        /// </summary>
+        /// <param name="type">The type identifying the context.</param>
+        /// <param name="context">The context to be registered.</param>
+        private static void Register(Type type, Context context)
+        {
+            if (!Contexts.ContainsKey(type))
+                Contexts.Add(type, new());
+
+            Contexts[type].Add(context);
         }
     }
 }
