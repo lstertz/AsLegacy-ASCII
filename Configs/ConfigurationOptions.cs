e96bdf278e0221081e0d8a672de70f9f0970214e
lstertz <lucas@lucasstertz.com>
2022-01-03 21:57:53 -0500

    112: Added the ConfigurationOptions Context.

---
+++ Configs/ConfigurationOptions.cs
@@ -0,0 +1,27 @@
+﻿using ContextualProgramming;
+
+namespace AsLegacy.Configs
+{
+    /// <summary>
+    /// The configuration options that can be loaded to define the 
+    /// functionality of the game.
+    /// </summary>
+    [Context]
+    public class ConfigurationOptions
+    {
+        /// <summary>
+        /// The user-friendly names of the available configurations.
+        /// </summary>
+        public ContextStateList<string> AvailableConfigurations { get; init; }
+
+        /// <summary>
+        /// The file paths of the available configurations.
+        /// </summary>
+        public ContextStateList<string> ConfigurationFiles { get; init; }
+
+        /// <summary>
+        /// The configuration currently chosen to define the game's functionality.
+        /// </summary>
+        public ContextState<string> CurrentConfiguration { get; init; }
+    }
+}
